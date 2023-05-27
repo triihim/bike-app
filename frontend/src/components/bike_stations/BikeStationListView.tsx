9c8a80f053ed254045d08c4dbb2ed2f76cfa9c14
Tatu Riihimäki <tatu.riihimaki@gmail.com>
2023-05-27 14:42:18 +0300

    added bike station list view

---
+++ frontend/src/components/bike_stations/BikeStationListView.tsx
@@ -0,0 +1,57 @@
+import { usePage } from '../../common/hooks/usePage';
+import { PaginationControls } from '../common/PaginationControls';
+import { Spacer, SpacerDirection } from '../common/Spacer';
+import { HeadingWithLoader } from '../common/HeadingWithLoader';
+import { BikeStation } from './types';
+import { ListItem, ListItemCell } from '../common/ListItem';
+
+export const BikeStationListView = () => {
+  const pageSize = 10;
+  const { loading, page, hasMore, nextPage, previousPage, pageIndex, totalPageCount } = usePage<BikeStation>(
+    pageSize,
+    '/bike-station/page',
+  );
+
+  const showNoBikeStations = !loading && !page;
+  const hasData = page !== null;
+
+  const controls = (
+    <PaginationControls
+      onNext={nextPage}
+      onPrevious={previousPage}
+      nextDisabled={!hasMore || loading}
+      previousDisabled={pageIndex === 1 || loading}
+      currentPageIndex={pageIndex}
+      totalPages={totalPageCount}
+    />
+  );
+
+  return (
+    <div>
+      <HeadingWithLoader label="Bike Stations" loading={loading} />
+      {showNoBikeStations && <p>No bike stations available</p>}
+      {controls}
+      <Spacer direction={SpacerDirection.Vertical} size={1.5} />
+      {hasData &&
+        page.data.map((bikeStation) => <BikeStationListItem key={bikeStation.id} bikeStation={bikeStation} />)}
+      <Spacer direction={SpacerDirection.Vertical} size={1.5} />
+      {controls}
+    </div>
+  );
+};
+
+interface BikeStationListItemProps {
+  bikeStation: BikeStation;
+}
+
+const BikeStationListItem = ({ bikeStation }: BikeStationListItemProps) => {
+  console.log(bikeStation.operator, bikeStation.operator.length);
+  return (
+    <ListItem>
+      <ListItemCell label="Name" value={bikeStation.name} />
+      <ListItemCell label="Address" value={`${bikeStation.address}, ${bikeStation.city}`} />
+      <ListItemCell label="Capacity" value={bikeStation.capacity} />
+      <ListItemCell label="Operator" value={bikeStation.operator} />
+    </ListItem>
+  );
+};
---
+++ frontend/src/components/bike_stations/types.ts
@@ -0,0 +1,10 @@
+export interface BikeStation {
+  id: number;
+  name: string;
+  address: string;
+  city: string;
+  operator: string;
+  capacity: number;
+  x: number;
+  y: number;
+}
