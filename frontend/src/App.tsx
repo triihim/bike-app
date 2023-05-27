435dbdf6a1da7fe09a24fdcf01e632b71bedcee1
Tatu Riihimäki <tatu.riihimaki@gmail.com>
2023-05-27 07:09:21 +0300

    added first global styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,12 @@
+import { GlobalStyles } from './styles/GlobalStyles';
+
 function App() {
-  return <div className="App">Working</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <div>Working</div>
+    </>
+  );
 }
 
 export default App;
