3056ad0ed809d13b9fb074d0b528a87d0eea1abe
Tatu Riihimäki <tatu.riihimaki@gmail.com>
2023-05-27 14:48:17 +0300

    added drop shadow to the app layout

--- frontend/src/components/common/AppLayout.tsx
+++ frontend/src/components/common/AppLayout.tsx
@@ -14,4 +14,7 @@ const StyledAppLayout = styled.div`
   margin: 2rem auto;
   padding: 3rem;
   background: ${(props) => props.theme.colors.surface};
+  -webkit-box-shadow: 0px 5px 16px -8px rgba(0, 0, 0, 0.75);
+  -moz-box-shadow: 0px 5px 16px -8px rgba(0, 0, 0, 0.75);
+  box-shadow: 0px 5px 16px -8px rgba(0, 0, 0, 0.75);
 `;
