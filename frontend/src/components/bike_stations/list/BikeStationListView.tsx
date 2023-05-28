875cad929b591f25b9bd81ac40eb64dd81dfb83a
Tatu Riihimäki <tatu.riihimaki@gmail.com>
2023-05-28 18:31:29 +0300

    refactored bike station listing to use table for listing

--- frontend/src/components/bike_stations/list/BikeStationListView.tsx
+++ frontend/src/components/bike_stations/list/BikeStationListView.tsx
@@ -1,13 +1,13 @@
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { usePage } from '../../../common/hooks/usePage';
 import { HeadingWithLoader } from '../../common/HeadingWithLoader';
-import { ListItem, ListItemCell } from '../../common/ListItem';
 import { PaginationControls } from '../../common/PaginationControls';
 import { Spacer, SpacerDirection } from '../../common/Spacer';
 import { BikeStation } from '../types';
+import { Table } from '../../common/Table';
 
 export const BikeStationListView = () => {
-  const pageSize = 10;
+  const pageSize = 20;
   const { loading, page, hasMore, nextPage, previousPage, pageIndex, totalPageCount } = usePage<BikeStation>(
     pageSize,
     '/bike-station/page',
@@ -32,27 +32,35 @@ export const BikeStationListView = () => {
       <HeadingWithLoader label="Bike Stations" loading={loading} />
       {showNoBikeStations && <p>No bike stations available</p>}
       {controls}
-      <Spacer direction={SpacerDirection.Vertical} size={1.5} />
-      {hasData &&
-        page.data.map((bikeStation) => <BikeStationListItem key={bikeStation.id} bikeStation={bikeStation} />)}
-      <Spacer direction={SpacerDirection.Vertical} size={1.5} />
+      <Spacer direction={SpacerDirection.Vertical} size={3} />
+      {hasData && (
+        <Table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Address</th>
+              <th>Capacity</th>
+              <th>Operator</th>
+            </tr>
+          </thead>
+          <tbody>
+            {page.data.map((bikeStation) => (
+              <tr key={bikeStation.id}>
+                <td>
+                  <Link to={`/bike-stations/${bikeStation.id}`}>{bikeStation.name}</Link>
+                </td>
+                <td>
+                  {bikeStation.address}, {bikeStation.city}
+                </td>
+                <td>{bikeStation.capacity}</td>
+                <td>{bikeStation.operator}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
+      <Spacer direction={SpacerDirection.Vertical} size={3} />
       {controls}
     </div>
-  );
-};
-
-interface BikeStationListItemProps {
-  bikeStation: BikeStation;
-}
-
-const BikeStationListItem = ({ bikeStation }: BikeStationListItemProps) => {
-  const navigate = useNavigate();
-  return (
-    <ListItem onClick={() => navigate(`/bike-stations/${bikeStation.id}`)}>
-      <ListItemCell label="Name" value={bikeStation.name} />
-      <ListItemCell label="Address" value={`${bikeStation.address}, ${bikeStation.city}`} />
-      <ListItemCell label="Capacity" value={bikeStation.capacity} />
-      <ListItemCell label="Operator" value={bikeStation.operator} />
-    </ListItem>
   );
 };
