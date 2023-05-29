f51f8b019d3fe9e02cf0e6e7aa82d6fb6c3d1918
Tatu Riihimäki <tatu.riihimaki@gmail.com>
2023-05-29 19:35:10 +0300

    added helper scripts to further streamline startup

---
+++ start_app.ps1
@@ -0,0 +1,7 @@
+# Start backend
+Set-Location -Path ".\backend"
+npm run dev:container
+
+# Start frontend
+Set-Location -Path "..\frontend"
+npm start
---
+++ start_app.sh
@@ -0,0 +1,7 @@
+# Start backend
+cd "./backend"
+npm run dev:container
+
+# Start frontend
+cd "../frontend"
+npm start
