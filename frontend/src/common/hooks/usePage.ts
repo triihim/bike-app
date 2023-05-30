3f3134d5954e75059ed66d1c26f2f997d1713438
Tatu Riihimäki <tatu.riihimaki@gmail.com>
2023-05-30 21:45:09 +0300

    fixed bug where filtering and pagination did not work together due to skipping over filtered results

--- frontend/src/common/hooks/usePage.ts
+++ frontend/src/common/hooks/usePage.ts
@@ -1,23 +1,51 @@
-import { useState, useEffect } from 'react';
-import { PageResponse, usePageParams } from '../types';
+import { useState, useEffect, useRef } from 'react';
+import { FilterBy, OrderBy, PageResponse, usePageParams } from '../types';
 import { useGetRequest } from './useGetRequest';
+
+const emptyFilterValues = <T>(filterBy: FilterBy<T>) => filterBy.value.trim().length > 0;
+
+const appendFilterValue = <T>(queryString: string, filter: FilterBy<T>) =>
+  queryString + `&filterColumn=${filter.property.toString()}&filterValue=${filter.value}`;
+
+const buildOrderingQueryString = <T>(orderBy?: OrderBy<T>) =>
+  orderBy ? `&sortColumn=${orderBy.property.toString()}&sortDirection=${orderBy.direction}` : '';
+
+const buildFilteringQueryString = <T>(filterBy?: Array<FilterBy<T>>) =>
+  filterBy ? filterBy.filter(emptyFilterValues).reduce(appendFilterValue, '') : '';
+
+const buildPageRequest = <T>(
+  path: string,
+  start: number,
+  pageSize: number,
+  orderBy?: OrderBy<T>,
+  filterBy?: Array<FilterBy<T>>,
+) =>
+  `${path}?start=${start}&limit=${pageSize}${buildOrderingQueryString(orderBy)}${buildFilteringQueryString(filterBy)}`;
 
 export const usePage = <T>({ pageSize, requestPath, initialPage, orderBy, filterBy }: usePageParams<T>) => {
   const [start, setStart] = useState(0);
   const [hasMore, setHasMore] = useState(false);
   const [pageIndex, setPageIndex] = useState(initialPage || 1);
   const [totalPageCount, setTotalPageCount] = useState(0);
-
-  const orderingQuery = orderBy ? `&sortColumn=${orderBy.property.toString()}&sortDirection=${orderBy.direction}` : '';
-  const filteringQuery = filterBy
-    ? filterBy.reduce(
-        (query: string, filter) => query + `&filterColumn=${filter.property.toString()}&filterValue=${filter.value}`,
-        '',
-      )
-    : '';
-
-  const request = `${requestPath}?start=${start}&limit=${pageSize}${orderingQuery}${filteringQuery}`;
+  const [request, setRequest] = useState(`${requestPath}?start=${start}&limit=${pageSize}`);
+  const filteringChanged = useRef(false);
   const { loading, data: page, error } = useGetRequest<PageResponse<T>>(request);
+
+  useEffect(() => {
+    filteringChanged.current = true;
+  }, [filterBy]);
+
+  useEffect(() => {
+    let newRequest: string;
+    if (filteringChanged.current === true) {
+      filteringChanged.current = false;
+      newRequest = buildPageRequest(requestPath, 0, pageSize, orderBy, filterBy);
+      goToPage(1);
+    } else {
+      newRequest = buildPageRequest(requestPath, start, pageSize, orderBy, filterBy);
+    }
+    setRequest(newRequest);
+  }, [pageSize, requestPath, orderBy, filterBy, start, pageIndex]);
 
   useEffect(() => {
     if (page && page.count) {
@@ -25,6 +53,12 @@ export const usePage = <T>({ pageSize, requestPath, initialPage, orderBy, filter
       setTotalPageCount(Math.ceil(page.count / pageSize));
     }
   }, [page]);
+
+  useEffect(() => {
+    if (pageIndex > totalPageCount) {
+      setPageIndex(Math.max(1, totalPageCount));
+    }
+  }, [totalPageCount]);
 
   const nextPage = () => {
     goToPage(pageIndex + 1);
