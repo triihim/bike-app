8ec81c75810a6b3310636133bd6a6d05eda20920
Tatu Riihimäki <tatu.riihimaki@gmail.com>
2023-06-02 12:39:19 +0300

    refactored existing tests to work with refactored backend

--- backend/test/parsing/bikeStationParsing.test.ts
+++ backend/test/parsing/bikeStationParsing.test.ts
@@ -6,7 +6,7 @@ describe('Bike station CSV parsing', () => {
   const csvParserBufferSize = 100; // This does not matter here, since testing csv's are small.
 
   it('discards rows missing any of the following: id, address, name, capacity, x, y', async () => {
-    const csvPath = path.resolve(__dirname, '../test_data/bike_stations/invalid5.csv');
+    const csvPath = path.resolve(__dirname, '../test_data/for_parsing/bike_stations/invalid5.csv');
     const parser = new CsvParser(csvPath, BIKE_STATION_COLUMN_HEADERS, bikeStationMapFn);
     await parser.parse({
       bufferSize: csvParserBufferSize,
@@ -17,7 +17,7 @@ describe('Bike station CSV parsing', () => {
   });
 
   it('fallbacks to Helsinki if city is missing', async () => {
-    const csvPath = path.resolve(__dirname, '../test_data/bike_stations/missing_city.csv');
+    const csvPath = path.resolve(__dirname, '../test_data/for_parsing/bike_stations/missing_city.csv');
     const parser = new CsvParser(csvPath, BIKE_STATION_COLUMN_HEADERS, bikeStationMapFn);
     await parser.parse({
       bufferSize: csvParserBufferSize,
@@ -29,7 +29,7 @@ describe('Bike station CSV parsing', () => {
   });
 
   it('does not discard rows containing all of the following: id, address, name, capacity, x, y', async () => {
-    const csvPath = path.resolve(__dirname, '../test_data/bike_stations/valid10.csv');
+    const csvPath = path.resolve(__dirname, '../test_data/for_parsing/bike_stations/valid10.csv');
     const parser = new CsvParser(csvPath, BIKE_STATION_COLUMN_HEADERS, bikeStationMapFn);
     await parser.parse({
       bufferSize: csvParserBufferSize,
