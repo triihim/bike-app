6276cf2da18fe0149c304cd6b7ee423c25f8c3ed
Tatu Riihimäki <tatu.riihimaki@gmail.com>
2023-06-04 19:41:27 +0300

    added backend initialisation status endpoint

--- backend/src/server/server.ts
+++ backend/src/server/server.ts
@@ -1,4 +1,4 @@
-import express, { Express } from 'express';
+import express, { Express, Request, Response } from 'express';
 import AppConfig from '../config';
 import bikeStationRouter from './routers/bikeStationRouter';
 import { errorHandler } from './middleware/errorHandler';
@@ -16,9 +16,11 @@ export class Server {
   private _app: Express;
   private _server: http.Server;
   private _configuration: ServerConfiguration;
+  private _initialised: boolean;
 
   constructor(configuration: ServerConfiguration) {
     this._configuration = configuration;
+    this._initialised = false;
   }
 
   // Exposed for testing
@@ -28,6 +30,7 @@ export class Server {
 
   async start() {
     await this.initialise();
+    this._initialised = true;
     this._server = this._app.listen(AppConfig.SERVER_PORT, () =>
       console.log(`Server running on port ${AppConfig.SERVER_PORT}`),
     );
@@ -43,12 +46,17 @@ export class Server {
     );
   }
 
+  private handleInitialisationStatusRequest = (req: Request, res: Response) => {
+    res.json({ initialised: this._initialised });
+  };
+
   private async initialise() {
     this._app = express();
     this._app.use(cors());
     this._app.use(express.json());
     this._app.use('/bike-stations', bikeStationRouter);
     this._app.use('/journeys', journeyRouter);
+    this._app.get('/initialisation-status', this.handleInitialisationStatusRequest);
     this._app.use(errorHandler);
     await initialiseDatabase(this._configuration.csvRootFolderPath);
   }
