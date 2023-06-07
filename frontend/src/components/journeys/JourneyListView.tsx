91c35f54cedf5918b90f601984f821401cb45a0a
Tatu Riihimäki <tatu.riihimaki@gmail.com>
2023-06-07 09:41:09 +0300

    added journey creation modal

--- frontend/src/components/journeys/JourneyListView.tsx
+++ frontend/src/components/journeys/JourneyListView.tsx
@@ -9,12 +9,16 @@ import { Link } from 'react-router-dom';
 import { useContext, useEffect, useState } from 'react';
 import { FilterBy, OrderBy } from '../../common/types';
 import { NotificationContext, NotificationType } from '../Notification';
+import { PrimaryButton } from '../common/PrimaryButton';
+import { ModalContext } from '../Modal';
+import { JourneyModal } from './JourneyModal';
 
 export const JourneyListView = () => {
   const pageSize = 20;
   const [orderBy, setOrderBy] = useState<OrderBy<Journey>>({ property: 'departureStationName', direction: 'ASC' });
   const [filters, setFilters] = useState<Array<FilterBy<Journey>>>([]);
   const { showNotification } = useContext(NotificationContext);
+  const { setModalContent } = useContext(ModalContext);
 
   const { loading, page, hasMore, nextPage, previousPage, pageIndex, totalPageCount, error } = usePage<Journey>({
     pageSize,
@@ -53,9 +57,14 @@ export const JourneyListView = () => {
     );
   };
 
+  const onAddJourneyClick = () => {
+    setModalContent(<JourneyModal onCancel={() => setModalContent(null)} />);
+  };
+
   return (
     <div>
       <HeadingWithLoader label="Journeys" loading={loading} />
+      <PrimaryButton onClick={onAddJourneyClick}>Add journey</PrimaryButton>
       {controls}
       <Spacer direction={SpacerDirection.Vertical} size={3} />
       <JourneyTable rows={page?.data} orderBy={orderBy} orderByColumn={orderByColumn} filterByValue={filterByValue} />
