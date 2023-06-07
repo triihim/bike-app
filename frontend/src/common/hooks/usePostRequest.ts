eca4c7f06cb5e4be327c2867703e1f68570af824
Tatu Riihimäki <tatu.riihimaki@gmail.com>
2023-06-07 14:55:37 +0300

    fixed usePostRequest not resetting its errors on new post request

--- frontend/src/common/hooks/usePostRequest.ts
+++ frontend/src/common/hooks/usePostRequest.ts
@@ -1,14 +1,15 @@
 import axios from 'axios';
 import { useState } from 'react';
 
-export const usePostRequest = <T>(path: string) => {
+export const usePostRequest = <TResponse>(path: string) => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
   const post = async (data: object) => {
+    setLoading(true);
+    setError(null);
     try {
-      setLoading(true);
-      return await axios.post<T>(path, JSON.stringify(data), {
+      return await axios.post<TResponse>(path, JSON.stringify(data), {
         baseURL: process.env.REACT_APP_API_BASE_URL,
         headers: { 'Content-Type': 'application/json' },
       });
