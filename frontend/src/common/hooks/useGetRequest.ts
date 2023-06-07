eec88f685970116addaec29b24f3af0068866eb8
Tatu Riihimäki <tatu.riihimaki@gmail.com>
2023-06-07 14:54:19 +0300

    refactored useGetRequest to allow refetching on demand

--- frontend/src/common/hooks/useGetRequest.ts
+++ frontend/src/common/hooks/useGetRequest.ts
@@ -6,22 +6,23 @@ export const useGetRequest = <TResponse>(path: string) => {
   const [error, setError] = useState<string | null>(null);
   const [data, setData] = useState<TResponse | null>(null);
 
-  useEffect(() => {
+  const fetch = async () => {
     setLoading(true);
     setError(null);
-    axios
-      .get<TResponse>(path, { baseURL: process.env.REACT_APP_API_BASE_URL })
-      .then((response) => {
-        setData(response.data);
-      })
-      .catch((error) => {
-        console.error(error);
-        setError('GET Request failed');
-      })
-      .finally(() => {
-        setLoading(false);
-      });
+    try {
+      const response = await axios.get<TResponse>(path, { baseURL: process.env.REACT_APP_API_BASE_URL });
+      setData(response.data);
+    } catch (error) {
+      console.error(error);
+      setError('GET Request failed');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetch();
   }, [path]);
 
-  return { loading, data, error };
+  return { loading, data, error, refetch: fetch };
 };
